b1e098d47a8d456db06db7e0c8453d7d2e5b55a6
michalzajac8338 <michal.zajac8338@gmail.com>
2023-10-18 15:30:21 +0200

    duplicates deleted

--- JavaProjects/BeforeSpringProjects/CourseProjects/todo-management/src/main/java/com/michal/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.michal.todo.repository;
-
-import com.michal.todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
